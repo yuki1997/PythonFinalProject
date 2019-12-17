3ca09319d97e8740936caebe9e91a98dd01cf0e7
AlanBai <35803288+yuki1997@users.noreply.github.com>
2019-12-17 17:00:20 +0800

    crawled all the href in 2016

--- rank/ranks.py
+++ rank/ranks.py
@@ -2,10 +2,11 @@ import requests
 from lxml import etree
 import pymysql
 
-res = requests.get('https://kaoshi.china.com/gaokao/news/1723427-1.htm').text
-html = etree.HTML(res)
+res = requests.get('https://kaoshi.china.com/gaokao/news/1723427-1.htm')
+res.encoding = 'utf-8'
+html = etree.HTML(res.text)
 href = html.xpath('/html/body/div[3]/div/div[1]/div[3]/ul/li/p/a/@href')
-print(href)
+
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='591586',
@@ -17,7 +18,7 @@ for i in range(0, len(href)):
     try:
         cursor.execute(sql, href[i])
         conn.commit()
-    except:
+    except Exception as e:
+        print(e)
         conn.rollback()
 conn.close()
-
